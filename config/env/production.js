4e85d1b9814a021ce8884e1fad55cfb329386532
R.ROGUI <rabii.rogui1@gmail.com>
2021-11-24 18:09:24 +0100

    commit local

--- config/env/production.js
+++ config/env/production.js
@@ -134,7 +134,7 @@ module.exports = {
   * If web browsers will be communicating with your app, be sure that        *
   * you have CSRF protection enabled.  To do that, set `csrf: true` over     *
   * in the `config/security.js` file (not here), so that CSRF app can be     *
-  * tested with     CSRF protection turned on in development mode too.           *
+  * tested with      CSRF protection turned on in development mode too.           *
   *                                                                          *
   ***************************************************************************/
  
