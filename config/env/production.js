769db81bc5c50eedf6d42b621ba58ebbfccad679
R.ROGUI <rabii.rogui1@gmail.com>
2021-11-24 15:05:41 +0100

    commit local

--- config/env/production.js
+++ config/env/production.js
@@ -59,6 +59,9 @@ module.exports = {
       //  ```
       //--------------------------------------------------------------------------
 
+      
+
+
       /****************************************************************************
       *                                                                           *
       * More adapter-specific options                                             *
@@ -135,10 +138,7 @@ module.exports = {
   *                                                                          *
   ***************************************************************************/
  
- sockets: {
-  onlyAllowOrigins: [
-  'http://localhost:1337',
-  ]  },
+ 
    security: {
 
     /***************************************************************************
@@ -346,6 +346,11 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   // port: 80,
+  host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+environment: process.env.NODE_ENV || 'development',
+explicitHost: process.env.HOST || 'logmodule-fuseki-container.146.59.159.175.nip.io/',
+
 
 
 
