d49aa9c1941b8ae1fe10f1cfd4ebafa26d7a1510
R.ROGUI <rabii.rogui1@gmail.com>
2021-11-26 11:03:49 +0100

    commit local

--- config/env/production.js
+++ config/env/production.js
@@ -346,9 +346,9 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   // port: 80,
-  port:   1439,
+  port:   8080,
 environment: process.env.NODE_ENV || 'development',
-explicitHost:   'logproject-fuseki-container.146.59.159.175.nip.io',
+explicitHost:   'log-abirsaid.146.59.159.175.nip.io ',
 
 
 
